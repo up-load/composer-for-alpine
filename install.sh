66f0ca47cceb87c592e6f8d324b971627c990529
up-load <38355121+up-load@users.noreply.github.com>
2018-11-08 05:33:35 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+apk --no-cache add git openssh php curl php7-curl php7-cli php7-fpm php7-json php7-phar php7-openssl php7-iconv php7-mbstring
+PHP_FPM_USER="www-data"
+PHP_FPM_GROUP="www-data"
+PHP_FPM_LISTEN_MODE="0660"
+sed -i "s|;listen.owner\s*=\s*nobody|listen.owner = ${PHP_FPM_USER}|g" /etc/php7/php-fpm.conf
+sed -i "s|;listen.group\s*=\s*nobody|listen.group = ${PHP_FPM_GROUP}|g" /etc/php7/php-fpm.conf
+sed -i "s|;listen.mode\s*=\s*0660|listen.mode = ${PHP_FPM_LISTEN_MODE}|g" /etc/php7/php-fpm.conf
+sed -i "s|user\s*=\s*nobody|user = ${PHP_FPM_USER}|g" /etc/php7/php-fpm.conf
+sed -i "s|group\s*=\s*nobody|group = ${PHP_FPM_GROUP}|g" /etc/php7/php-fpm.conf
+adduser -D -g 'www-data' www-data
+mkdir /www/
+chown -R www-data:www-data /www/
+cd /www/
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
